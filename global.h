70eb027ab35abacd60e357406cf6c5599ce087d6
Maged ZeaRBaN <50809218+ZeaRBaN@users.noreply.github.com>
2022-05-24 01:08:45 +0200

    Add files via upload

---
+++ global.h
@@ -0,0 +1,15 @@
+#ifndef GLOBAL_H_INCLUDED
+#define GLOBAL_H_INCLUDED
+
+struct info
+{ char fname[30];
+ char lname[30];
+ int number;
+ char mail[50];
+ char address[100];
+
+} ;
+
+
+#endif // GLOBAL_H_INCLUDED
+
